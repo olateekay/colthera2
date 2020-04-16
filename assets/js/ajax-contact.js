94b295e7970f178550f46515915a7841a6bed706
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2020-04-17 00:26:24 +0100

    more bug fixes

--- assets/js/ajax-contact.js
+++ assets/js/ajax-contact.js
@@ -1,39 +1,38 @@
 (function ($) {
-    'use strict';
+  "use strict";
 
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
+  var form = $(".contact__form"),
+    message = $(".contact__msg"),
+    form_data;
 
-    // Success function
-    function done_func(response) {
-        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
-        message.text('Your message was sent successfully.');
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
+  // Success function
+  function done_func(response) {
+    message.fadeIn().removeClass("alert-danger").addClass("alert-success");
+    message.text("Your message was sent successfully.");
+    setTimeout(function () {
+      message.fadeOut();
+    }, 2000);
+    form.find('input:not([type="submit"]), textarea').val("");
+  }
 
-    // fail function
-    function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-danger');
-        message.text('Your message not was sent successfully.');
-        setTimeout(function () {
-            message.fadeOut();
-        }, 20000);
-    }
-    
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-        .done(done_func)
-        .fail(fail_func);
-    });
-    
+  // fail function
+  function fail_func(data) {
+    message.fadeIn().removeClass("alert-success").addClass("alert-danger");
+    message.text("Your message was not sent successfully.");
+    setTimeout(function () {
+      message.fadeOut();
+    }, 20000);
+  }
+
+  form.submit(function (e) {
+    e.preventDefault();
+    form_data = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form_data,
+    })
+      .done(done_func)
+      .fail(fail_func);
+  });
 })(jQuery);
